f3d21e07b83a85f3d63d32e9ed68c271aaba0333
vishnukantkendre <104567950+vishnukantkendre@users.noreply.github.com>
2023-06-24 14:36:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get the width of the viewport
+var viewportWidth = window.innerWidth;
+
+// Media query for small screens
+if (viewportWidth <= 768) {
+  // Hide the education and skills sections
+  document.querySelector(".education").style.display = "none";
+  document.querySelector(".skills").style.display = "none";
+}
+
+// Media query for medium screens
+if (viewportWidth <= 992) {
+  // Hide the contact section
+  document.querySelector(".contact").style.display = "none";
+}
